bb10c37f8f254506b9ed53c2f588a509bbe6bf98
Kat <kat.m.deans@gmail.com>
2025-01-14 18:47:22 -0500

    Editor initial styling

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
@@ -7,9 +9,13 @@ export const Editor = () => {
         extensions: [StarterKit],
         content: '<p>Hello World! 🌎️</p>',
       })
+
+    
     return (
-        <div>
-            <EditorContent  editor={editor} />
+        <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+                <EditorContent  editor={editor} />
+            </div>
         </div>
     );
 };
