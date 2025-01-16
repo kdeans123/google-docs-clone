2e5854cb3eb3936e05f96f982561772a22214e96
Kat <kat.m.deans@gmail.com>
2025-01-15 20:44:30 -0500

    Tables

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,12 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
 
 
 export const Editor = () => {
@@ -12,7 +18,17 @@ export const Editor = () => {
                 class:"focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             }
         },
-        extensions: [StarterKit],
+        extensions: [
+            StarterKit,
+            Table,
+            TableCell,
+            TableHeader,
+            TableRow,
+            TaskItem.configure({
+                nested: true,
+              }),
+            TaskList,
+        ],
         content: '<p>Hello World! 🌎️</p>',
       })
 
